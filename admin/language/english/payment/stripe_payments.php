1b6b2a3da3fc808c1d51df8d5f3ec8afd2ef30f6
Andrei V <avarabyou@gmail.com>
2014-10-03 16:42:20 -0400

    Stripe extension for Opencart Initial Commit

---
+++ admin/language/english/payment/stripe_payments.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']             = 'Stripe Payments';
+
+// Text 
+$_['text_payment']              = 'Payment';
+$_['text_success']              = 'Success: You have modified Stripe Payments account details!';
+$_['text_stripe_payments'] = '<a href="http://www.stripe.com/" target="_blank"><img src="view/image/payment/stripe-logo.png" alt="Stripe" title="Stripe" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_test']                 = 'Test';
+$_['text_live']                 = 'Live';
+$_['text_authorization']        = 'Authorization';
+$_['text_charge']               = 'Charge';
+
+// Entry
+$_['entry_public']              = 'Public Key:';
+$_['entry_key']                 = 'Secret Key:';
+$_['entry_mode']                = 'Transaction Mode:';
+$_['entry_method']              = 'Transaction Method:';
+$_['entry_total']               = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
+$_['entry_order_status']        = 'Order Status:';
+$_['entry_geo_zone']            = 'Geo Zone:'; 
+$_['entry_status']              = 'Status:';
+$_['entry_sort_order']          = 'Sort Order:';
+
+// Error 
+$_['error_permission']          = 'Warning: You do not have permission to modify payment: Stripe Payments!';
+$_['error_public']              = 'Public Key Required!';
+$_['error_key']                 = 'Secret Key Required!';
+?>
